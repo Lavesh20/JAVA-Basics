dfe6f61d1e8f1faecd08be733570e62377074fb8
unknown <laveshvyas20@gmail.com>
2024-02-20 10:14:58 +0530

    operators

--- Operators/src/Main.java
+++ Operators/src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       //Addition Operators
+        int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+        System.out.println("The addition of two numbers are: " + sum);
+
+        //Subtraction
+        int sub = num2 - num1;
+        System.out.println("The substraction of two numbers are: "+sub);
+
+        //Multiplication
+        int multiply = num1*num2;
+        System.out.println("Multiplication: "+multiply);
+
+        //Division
+        int div = num2/num1;
+        System.out.println("Division: "+div);
+
+        //Modulas operator
+        int rem = num2%num1;
+        System.out.println("Remainder: "+rem);
     }
 }
