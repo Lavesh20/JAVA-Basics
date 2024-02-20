b173c26dded05be8e78c07482d65a32ebeda8658
unknown <laveshvyas20@gmail.com>
2024-02-20 15:01:36 +0530

    Added conditional statement

--- ConditionalStatement/src/Main.java
+++ ConditionalStatement/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        if(age == 20){
+            System.out.println("Perfect");
+        }
+        else if(age<20){
+            System.out.println("Young");
+        }
+        else if (age > 20 && age<50) {
+            System.out.println("Elder");
+        }
+        else{
+            System.out.println("Old");
+        }
     }
 }
