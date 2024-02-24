666b6c8f1de323517f4956b45bc18e25da52eec9
unknown <laveshvyas20@gmail.com>
2024-02-24 08:33:39 +0530

    Function in Java

---
+++ Functions/src/MathLibrary.java
@@ -0,0 +1,17 @@
+
+public class MathLibrary {
+    public static void main(String[] args) {
+        int  a = 10;
+        int b = 7;
+
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.sqrt(100));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.abs(-10));
+        System.out.println(Math.floor(Math.random()*10));
+        System.out.println(Math.round(Math.random()*10));
+        System.out.println(Math.round(5.5));
+
+    }
+}
